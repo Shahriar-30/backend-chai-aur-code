18dd0f0039d5fbeb41e935d856c1edd7b6e92ab6
shahriar-30 <alsiphat30@gmail.com>
2026-02-13 22:18:13 +0600

    mongodb connected

---
+++ src/datebase/index.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    const db = await mongoose.connect(`${process.env.MONGO_URL}/${process.env.DB_NAME}`);
+    console.log('MongoDB Connected');
+  } catch (error) {
+    console.error('MongoDB connection error ', error);
+    throw error;
+  }
+};
+
+export default connectDB;
