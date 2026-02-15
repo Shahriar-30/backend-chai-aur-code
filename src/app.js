08cdc5fac5dd0d6435fbb334673fea90b7321c23
shahriar-30 <alsiphat30@gmail.com>
2026-02-15 12:34:34 +0600

    add cookie-parser and cors middleware; implement user registration route and controller

--- src/app.js
+++ src/app.js
@@ -1,5 +1,24 @@
+import cookieParser from "cookie-parser";
 import express from "express";
+import cors from "cors";
 
 let app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "*",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+// import routers
+import userRouter from "./routes/user.route.js";
+
+// use routers
+app.use("/api/v1/users", userRouter);
 
 export { app };
